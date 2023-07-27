dab85cbc8d94cc375237b1992e6fd59b9f8253c2
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2023-07-27 14:50:33 +0700

    connect to mongodb

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/", indexRouter);
 
+// connect mongodb
+mongoose.connect(`${env.MONGODB_URI}${env.MONGODB_HOST}:${env.MONGODB_PORT}`, {
+  dbName: env.MONGODB_DB_NAME,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function () {
+  console.log("Connected MongoDB");
+});
+
 app.listen(env.APP_PORT, () => {
   console.log(`Server is Running in port ${env.APP_PORT}`);
 });
